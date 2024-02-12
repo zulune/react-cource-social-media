64f1aaedd4b4c30d29a3ff4d6d5c14b21b899a8c
MAC <mars.xaker@gmail.com>
2024-02-12 16:41:39 +0200

    setting up firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKtWCe4KVuQnzfN3-JKP9-YG6HJ6_SERU",
+  authDomain: "react-cource-46cfb.firebaseapp.com",
+  projectId: "react-cource-46cfb",
+  storageBucket: "react-cource-46cfb.appspot.com",
+  messagingSenderId: "434108594706",
+  appId: "1:434108594706:web:67fc229a7977c17d3af6cb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+    return (
+        <div>
+            <h1>Welcome to Login Page</h1>
+        </div>
+    )
+}
